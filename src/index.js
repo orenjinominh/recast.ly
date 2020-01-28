458f9e242ea2dfd7a0284a568faeb47e7471c153
Justin Anderson <justand@gmail.com>
2020-01-28 13:06:26 -0800

    Begin sprint

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 // TODO: Render the `App` component to the DOM
+
+ReactDOM.render(<App />, document.getElementById("app"));
+
 import App from './components/App.js';
+
+// ReactDOM.render(<App />, document.getElementById("actual-dom-element-where-I-want-to-render-my-component"));
 
