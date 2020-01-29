2c1f14034de4fd8685b6603bc49d6aae3de75c7f
Justin Anderson <justand@gmail.com>
2020-01-29 12:09:34 -0800

    Complete creating a dynamic Video Player component

--- src/components/VideoListEntry.jsx
+++ src/components/VideoListEntry.jsx
@@ -1,4 +1,4 @@
-class VideoListEntry extends React.Component {
+/*class VideoListEntry extends React.Component {
   constructor(props) {
     super(props);
   }
@@ -21,24 +21,24 @@ class VideoListEntry extends React.Component {
     );
   }
 }
+*/
 
-/*
 var VideoListEntry = (props) => (
   <div className="video-list-entry media">
     <div className="media-left media-middle">
-      <img className="media-object" src={this.props.video.snippet.thumbnails.default.url} alt=""/>
+      <img className="media-object" src={props.video.snippet.thumbnails.default.url} alt=""/>
     </div>
     <div className="media-body">
       <div className="video-list-entry-title">
-        {this.props.video.snippet.title}
+        {props.video.snippet.title}
       </div>
       <div className="video-list-entry-detail">
-        {this.props.video.snippet.description}
+        {props.video.snippet.description}
       </div>
     </div>
   </div>
 );
-*/
+
 
 // PropTyes tell other developers what `props` a component expects
 // Warnings will be shown in the console when the defined rules are violated
--- src/start.sh
+++
@@ -1,1 +0,0 @@
-babel --watch . --out-dir compiled --presets=es2015,react --ignore=node_modules,compiled --source-maps inline
