f7615cf7dc3fcdbad9ef8634007d691a34a9ce6a
Justin Anderson <justand@gmail.com>
2020-01-28 20:37:39 -0800

    Complete rendering App component to the DOM

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
-// TODO: Render the `App` component to the DOM
 
 ReactDOM.render(<App />, document.getElementById('app'));
 
 import App from './components/App.js';
 
-// ReactDOM.render(<App />, document.getElementById("actual-dom-element-where-I-want-to-render-my-component"));
 
